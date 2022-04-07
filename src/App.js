d644351a5ab33a095d42d35a5c37c28fbc581c70
Juan Albertsen <albertsenjuanm@gmail.com>
2022-04-06 22:44:13 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,18 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h2>¡Bienvenidos a la tienda Music Box!</h2>
+      <p>Espacio en Construcción</p>
+      <img src='logo192.png'></img>
       </header>
     </div>
   );
