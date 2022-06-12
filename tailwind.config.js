876547054fbc0490d01584c10fa0315538b5a821
Juan Albertsen <albertsenjuanm@gmail.com>
2022-06-12 18:55:52 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,6 @@ theme: {
 plugins: [require("daisyui")],
 
 daisyui: {
-  themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+  themes: ["cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
 },
 }
