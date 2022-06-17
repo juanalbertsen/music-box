6167a3450d768c26b8955eb2e43802dc19646f6e
Juan Albertsen <85770676+juanalbertsen@users.noreply.github.com>
2022-06-17 15:59:47 -0300

    Update ItemList.js

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -5,7 +5,7 @@ function ItemList(props) {
 
      return (
        <>
-            <div className=" mx-auto grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 m-8">
+            <div className=" mx-auto w-4/5 grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 m-8">
               {props.items.map( i => <Item key={i.id} item={i}></Item>)}
             </div>
         </>
