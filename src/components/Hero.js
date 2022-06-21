32b4eeeed2ec103aea3d39e138ba47274acfcb8e
Juan Albertsen <85770676+juanalbertsen@users.noreply.github.com>
2022-06-20 23:42:49 -0300

    update home

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,32 @@
 import React from 'react'
+// import Swiper core and required modules
+import { Navigation, Pagination,} from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/scrollbar';
 
 const Hero = () => {
   return (
-    <div>
-      <img className="w-4/5 mt-4 mx-auto"src="./img/hero.png"></img>
-    </div>
-  )
+    <Swiper className='w-4/5'
+      modules={[Navigation]}
+      spaceBetween={50}
+      slidesPerView={1}
+      navigation
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>
+      <img className="w-full mt-4 mx-auto" src="./img/hero.svg"></img>
+      </SwiperSlide>
+      <SwiperSlide>
+      <img className="w-full mt-4 mx-auto" src="./img/hero2.svg"></img>
+      </SwiperSlide>
+    </Swiper>
+  );
 }
 
 export default Hero
